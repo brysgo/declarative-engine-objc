d6c536d6cf0ede74a858924c1efe6d03bd61d302
Bryan Goldstein <brysgo@gmail.com>
2019-04-07 14:54:57 -0400

    run specta tests

---
+++ Example/Pods/Target Support Files/Pods-declarative-engine_Tests/Pods-declarative-engine_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_declarative_engine_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_declarative_engine_Tests
+@end
---
+++ Example/Pods/Target Support Files/declarative-engine/declarative-engine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_declarative_engine : NSObject
+@end
+@implementation PodsDummy_declarative_engine
+@end
